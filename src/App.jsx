a8a3b6fe3f388e244fb64d8f0af2229a9a64ad99
rizwandakeysha <rizwandakeysha@gmail.com>
2025-04-10 21:33:22 +0700

    Update minor

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,10 @@ import Champions from "./components/User/Champions";
 import Seniman from "./components/User/Seniman";
 import KategoriPage from "./components/User/KategoriPage";
 import DetailKarya from "./components/User/DetailKarya";
-import NotFound from "./components/NotFound"; // Halaman 404
-import Layout1 from "./components/resources/Layout"; // Layout yang berisi background
+import NotFound from "./components/NotFound";
+import Layout1 from "./components/resources/Layout";
 
 function App() {
-  // Fungsi untuk mengecek apakah cookies 'visitorName' sudah ada
   const getCookie = (name) => {
     const match = document.cookie.match(
       new RegExp("(^| )" + name + "=([^;]+)")
@@ -31,23 +30,23 @@ function App() {
     return match ? match[2] : null;
   };
 
-  // Mengecek apakah visitor sudah pernah mengisi form
   const visitorName = getCookie("visitorName");
 
   return (
     <Router>
       <Layout1>
         <Routes>
-          {/* Halaman Utama */}
-          {!visitorName ? (
-            <Route path="/ddd" element={<VisitorForm />} />
-          ) : (
-            <Route path="/" element={<UserGallery />} />
-          )}
+          {/* Jika belum isi nama, redirect ke form */}
+          <Route
+            path="/"
+            element={
+              visitorName ? <UserGallery /> : <Navigate to="/visitorForm" replace />
+            }
+          />
 
           <Route path="/visitorForm" element={<VisitorForm />} />
 
-          {/* Halaman khusus admin */}
+          {/* Halaman admin */}
           <Route path="/admin-login" element={<AdminLogin />} />
           <Route
             path="/admin"
@@ -83,25 +82,16 @@ function App() {
           />
           <Route path="/admin/visitor" element={<VisitorList />} />
 
-          {/* Halaman Tambahan */}
-          <Route
-            path="/glimpse-of-artropolis"
-            element={<GlimpseOfArtropolis />}
-          />
+          {/* Halaman user */}
+          <Route path="/glimpse-of-artropolis" element={<GlimpseOfArtropolis />} />
           <Route path="/champions" element={<Champions />} />
           <Route path="/seniman" element={<Seniman />} />
-
-          {/* Rute dinamis dengan satu parameter kategori */}
           <Route path="/:kategori" element={<KategoriPage />} />
-
-          {/* Halaman Detail Karya */}
           <Route path="/:kategori/:judul_karya" element={<DetailKarya />} />
-
-          {/* Route wildcard untuk menangani path lainnya */}
-          <Route path="*" element={<NotFound />} />
 
           {/* Halaman 404 */}
           <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout1>
     </Router>
